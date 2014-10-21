094cc2ad48eb1bf48693acf6eff3d40de6b8de47
Thomas Lundqvist <biljettshop@users.noreply.github.com>
2014-10-21 15:22:53 +0200

    Update api.py
    Unicode fix.

--- trustly/api/api.py
+++ trustly/api/api.py
@@ -72,8 +72,10 @@ class API(object):
         elif type(data) == types.DictType:
             for k in sorted(data.keys(), key=lambda s: str(s)):
                 ret = ret + k + self.serialize_data(data[k])
+        elif type(data) == types.UnicodeType:
+                return unicode(data).encode('utf-8')
         elif data is not None:
-            return unicode(data).encode('utf-8')
+            return str(data)
         return ret
 
     def _verify_trustly_signed_data(self, method, uuid, signature, data):
